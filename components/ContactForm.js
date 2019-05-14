4fb43d9ce1ac49c542013bfdbfce847261e86924
Agnieszka Matyja <agmatyja@gmail.com>
2019-05-15 01:01:02 +0200

    form and contacts work

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,53 +1,28 @@
-var contacts = [
-  {
-    id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com',
-  },
-  {
-    id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
-  },
-  {
-    id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
-  }
-];
-
-var contactForm = {
-  firstName: '',
-  lastName: '',
-  email: ''
-};
-
-var App = React.createClass({
-  render: function() {
-    return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
-      )
-    );
-  }
-});
-
-var Contacts = React.createClass({
+var ContactForm = React.createClass({
   propTypes: {
-    items: React.PropTypes.array.isRequired,
+    contact: React.PropTypes.object.isRequired
   },
 
   render: function() {
-    var contacts = this.props.items.map(function(contact) {
-        return React.createElement(Contact, {item: contact, key: contact.id});
-    });
-
     return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
-    );
-  }
-});
+      React.createElement('form', {className: 'contactForm'},
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Imię',
+          value: this.props.contact.firstName,
+        }),
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Nazwisko',
+          value: this.props.contact.lastName,
+        }),
+        React.createElement('input', {
+          type: 'email',
+          placeholder: 'Email',
+          value: this.props.contact.email,
+        }),
+        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
+      )
+    )
+  },
+})
--- components/contacts.js
+++ components/contacts.js
@@ -25,13 +25,18 @@ var contactForm = {
   email: ''
 };
 
-var App = React.createClass({
+var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
   render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return React.createElement(Contact, {item: contact, key: contact.id});
+    });
+
     return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
-      )
+      React.createElement('ul', {className: 'contactsList'}, contacts)
     );
   }
 });
